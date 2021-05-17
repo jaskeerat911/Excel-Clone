7794e9ffa5aca9579a622892410eb8c4b5fdb07d
Jaskeerat Singh <jaskeerat911@gmail.com>
2021-05-18 00:27:47 +0530

    border fixed

--- script.js
+++ script.js
@@ -261,7 +261,7 @@ $("#border").change(function (e) {
     let value = $(this).val();
     $(".input-cell.selected").css("border", "");
     if (value == "none") {
-        $(".input-cell.selected").css("border", "");
+        $(".input-cell.selected").removeClass("border-outer border-left border-right border-top border-bottom");
     }
     else if (value == "outer") {
         $(".input-cell.selected").addClass("border-outer");
@@ -345,7 +345,7 @@ $(".color-pick").colorPick({
     'initialColor': '#TYPECOLOR',
     'allowRecent': true,
     'recentMax': 5,
-    'allowCustomColor': false,
+    'allowCustomColor': true,
     'palette': ["#1abc9c", "#16a085", "#2ecc71", "#27ae60", "#3498db", "#2980b9", "#9b59b6", "#8e44ad", "#34495e", "#2c3e50", "#f1c40f", "#f39c12", "#e67e22", "#d35400", "#e74c3c", "#c0392b", "#ecf0f1", "#bdc3c7", "#95a5a6", "#7f8c8d"],
     'onColorSelected': function() {
         if (this.color != "#TYPECOLOR") {
